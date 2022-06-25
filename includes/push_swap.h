8bc8e3b462f42a882706b3166f369bfea2fb2d71
Thitiwut Somsa <tsomsa@z3t3c4.42bangkok.com>
2022-06-25 23:19:05 +0700

    fix norm

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,22 +1,34 @@
-#include "ft_printf.h"
-#include "libft.h"
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomsa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 23:17:08 by tsomsa            #+#    #+#             */
+/*   Updated: 2022/06/25 23:17:10 by tsomsa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# include "ft_printf.h"
+# include "libft.h"
+# include <stdlib.h>
 
 # define MODE_A 1
 # define MODE_B 2
 # define MODE_S 3
 
-t_list  *g_lst1;
-t_list  *g_lst2;
+t_list	*g_lst1;
+t_list	*g_lst2;
 
 int		valid_int(char *str);
 int		valid_lst(t_list *lst);
 int		sorted_lst(t_list *lst);
-t_list*	set_var_nbs(int argc, char *argv[]);
-t_list*	set_var_str(char *str);
+t_list	*set_var_nbs(int argc, char *argv[]);
+t_list	*set_var_str(char *str);
 void	del_content(void *content);
 int		cint(t_list *lst);
 void	lst_print(t_list *lst1, t_list *lst2);
