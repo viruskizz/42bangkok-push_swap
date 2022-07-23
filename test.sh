516e3237e847325b5dbb6616175c46e30cd9d1f8
viruskizz <kizzaraiva@gmail.com>
2022-07-23 22:17:37 +0700

    update test

--- test.sh
+++ test.sh
@@ -2,7 +2,11 @@
 echo "size = $1"
 ARG=$(ruby -e "puts (1..$1).to_a.shuffle.join(' ')")
 echo "times =  $(./push_swap $ARG | wc -l)"
-./push_swap $ARG | ./checker_linux $ARG
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ./push_swap $ARG | ./checker_Mac $ARG
+else
+    ./push_swap $ARG | ./checker_linux $ARG
+fi
 if [ -n "$2" ]; then
     echo "detail"
     ./push_swap $ARG
